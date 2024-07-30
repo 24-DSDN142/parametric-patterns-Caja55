2b353188a78a90a9e7577ce200890a19a87042c1
Caja55 <hancoccaja@myvuw.ac.nz>
2024-07-31 02:13:06 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,5 +1,6 @@
 //your parameter variables go here!
 
+//so i can change my fish
 let headX = 0   //0 = middle
 let headY =0  //0= middle
 let eyeSize = 8 // 8 usually
@@ -8,22 +9,16 @@ let fishColour;
 let fishSize = 1
 let eyeColour;
 
-
-//if statements 
-let backgroundVar = 1
-// 0, basic fish
-//1, 
-
-let fishVar = 1 //1 fish 
-let bubbleVar = 2 // 1=normal bubbles 2=many small bubles
-
-
-//ignore me
-
 // bubble code 
 let bubbleX = 0
 let bubbleY = 0
 let bubbleSize = 10
+
+//bigger changes
+let fishVar = 1 //1 -fish layout for P1, 2 for P2 ect.
+let bubbleVar = 1 //1- no bubbles,2- detailed buble,3- plain bubble
+
+//ignore me
 //fish sizing code (so it moves together)
 let rectSize = 50 * fishSize;
 let mouthSize = 3 * fishSize;
@@ -35,8 +30,6 @@ let smallOvalHeight = 41 * fishSize;
 let tailSize = 25 * fishSize;
 let tailOffset = 10 * fishSize;
   
-
-
 function setup_wallpaper(pWallpaper) {
 
 // colours for fishy
@@ -44,12 +37,20 @@ bgColour = color(255, 251, 230);
 fishColour = color(0,8,51)
 eyeColour = color(153,25,0)
 
+//eye colour
+//P1 color(153,25,0)
+//P2
+
+
+
+
+
   angleMode(DEGREES);// for head and tail square
 
 
-  pWallpaper.output_mode(DEVELOP_GLYPH);
-  pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(true); //set this to false when you're ready to print
+  pWallpaper.output_mode(GRID_WALLPAPER);
+  pWallpaper.resolution(NINE_PORTRAIT);
+  pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
@@ -57,43 +58,52 @@ eyeColour = color(153,25,0)
   pWallpaper.grid_settings.row_offset  = 50;
 }
 
-
 function wallpaper_background() {
 
   background(bgColour); 
-
 }
+
+
+
+
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
   
 
-{//draw fish
 
-if (backgroundVar == 1){
-  DrawBubble(130,60)
-  DrawBubble(135,40)
-  DrawNew(0, 0, fishSize); 
+
+
+
+
+//fishy positions
+if (fishVar ==1){
+  DrawFish(-80, 0, fishSize, true); // Mirrored fish
+  DrawFish(-10, 0, fishSize, true);
+  DrawFish(60, 0, fishSize, true); 
+  DrawFish(130, 0, fishSize, true); 
 }
 
-if (backgroundVar ==2){
-  DrawNew(60, 100, fishSize); 
-  DrawNew(-10, 100, fishSize);
-  DrawNew(-80, 100, fishSize); 
-   
-  DrawNew(-80, 0, fishSize, true); // Mirrored fish
-  DrawNew(-10, 0, fishSize, true);
-  DrawNew(60, 0, fishSize, true); 
-  DrawNew(130, 0, fishSize, true); 
-   
-  DrawNew(60, -100, fishSize); 
-  DrawNew(-10, -100, fishSize);
-  DrawNew(-80, -100, fishSize); 
-
+if (fishVar ==2){
+  DrawFish(headX, headY, fishSize, true);
 }
 
 
 
 
+//Bubble stuff
+
+
+
+
+
+if (bubbleVar ==1){}
+if (bubbleVar ==2){
+  DrawBubble(bubbleX,bubbleY,bubbleSize)
+  DrawBubble(20,70,bubbleSize+2)
+  DrawBubble(50,90,bubbleSize+5)
+}
+if (bubbleVar ==3){
+  DrawBubble2(bubbleX,bubbleY,bubbleSize)
 }
 
 }
@@ -111,7 +121,7 @@ if (backgroundVar ==2){
 
 
 
-function DrawNew(headX,headY,fishSize, mirror = false){//fish face
+function DrawFish(headX,headY,fishSize, mirror = false){//fish face
   
   //fish face
     push();
@@ -173,18 +183,22 @@ function DrawNew(headX,headY,fishSize, mirror = false){//fish face
   }
 
 
-  function DrawBubble(bubbleX,bubbleY){
+  function DrawBubble(bubbleX,bubbleY,bubbleSize){
 
-    fill(fishColour)
+  fill(fishColour)
     circle(bubbleX ,bubbleY,bubbleSize)
     
     fill(255)
-    circle(bubbleX+2,bubbleY-2,bubbleSize-7)
-    circle(bubbleX+3,bubbleY,bubbleSize-8)
-    
+    circle(bubbleX+2,bubbleY-2,bubbleSize/4)
+    circle(bubbleX+3,bubbleY,bubbleSize/5)
     }
     
+function DrawBubble2(BubbleX,bubbleY,bubbleSize){
 
+  stroke(fishColour)
+  circle(bubbleX ,bubbleY,bubbleSize)
+
+}
 
 
 
